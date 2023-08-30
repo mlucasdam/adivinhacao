97df3eb5ab6b85b61676d255c303c99cdf26be4f
mlucasdam <damiani.lucasm@gmail.com>
2023-08-30 18:02:55 -0300

    avançando no codigo

--- adivinhação.c
+++ adivinhação.c
@@ -14,7 +14,21 @@ int main(){
     scanf("%d", &chute);
     printf("seu chute foi: %d \n", chute);
 
-
+    if (chute == secret_num){
+        printf("Você acertou!!\n");
+    }
     
+    else{
+
+        if(chute > secret_num){
+            printf("Você errou!!Tente novamente\n");
+            printf("Seu chute foi maior que o numero secreto.\n");
+        }
+
+        if (chute < secret_num){
+            printf("Você errou!!Tente novamente\n");
+            printf("Seu chute foi menor que o numero secreto.\n");
+        }
+    }
 }
 
