003f8d5b2f06f79134c7d530911f4536b280971a
mlucasdam <damiani.lucasm@gmail.com>
2023-08-30 17:25:24 -0300

    meu primeiro codigo em C

---
+++ adivinhação.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    // Imprime o cabeçalho do jogo 
+    printf("*********************************\n");
+    printf("*Bem-vindo ao jogo de advinhação*\n");
+    printf("*********************************\n");
+
+    int secret_num = 42;
+
+    printf("Meu numero secreto é: %d", secret_num);
+}
+
